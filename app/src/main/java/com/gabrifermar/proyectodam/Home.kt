ceff3eed1ae63d32c293bf25be1e34bc8bc47e88
gabrifermar <gabrifermar@gmail.com>
2021-10-07 11:37:02 +0200

    New fragment_login

--- app/src/main/java/com/gabrifermar/proyectodam/Home.kt
+++ app/src/main/java/com/gabrifermar/proyectodam/Home.kt
@@ -26,10 +26,6 @@ class Home : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarHome.toolbar)
 
-        binding.appBarHome.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_home)
@@ -37,7 +33,7 @@ class Home : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_login
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
