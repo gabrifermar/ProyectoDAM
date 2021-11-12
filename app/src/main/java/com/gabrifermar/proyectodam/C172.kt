84e3df5ee7c4d53854e8011e738920b3b2843866
gabrifermar <gabrifermar@gmail.com>
2021-11-12 01:19:04 +0100

    notificación al descargar manual C172

--- app/src/main/java/com/gabrifermar/proyectodam/C172.kt
+++ app/src/main/java/com/gabrifermar/proyectodam/C172.kt
@@ -1,10 +1,14 @@
 package com.gabrifermar.proyectodam
 
 import android.Manifest
-import android.app.DownloadManager
+import android.app.*
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -18,45 +22,89 @@ import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 import java.io.File
 import android.os.Environment.DIRECTORY_DOWNLOADS
+import android.widget.RemoteViews
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class C172 : AppCompatActivity() {
 
     private lateinit var binding: ActivityC172Binding
+    private lateinit var builder: Notification.Builder
+    private lateinit var notificationManager : NotificationManager
+    private lateinit var notificationChannel : NotificationChannel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityC172Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val storage = Firebase.storage
-        val ref =
-            storage.getReferenceFromUrl("gs://proyectoaep-d6bc6.appspot.com/Cessna-172N-POH.pdf")
-        val localfile = File.createTempFile("Cessna-172N-POH", "pdf")
+        //variables
+        notificationManager=getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
 
         //listeners
         binding.c172BtnPoh.setOnClickListener {
             //check for permission
             if (checkPermission()) {
-                val dn: DownloadManager =
-                    getSystemService(DOWNLOAD_SERVICE) as DownloadManager
-                val uri =
-                    Uri.parse("https://firebasestorage.googleapis.com/v0/b/proyectoaep-d6bc6.appspot.com/o/Cessna-172N-POH.pdf?alt=media&token=30bc84a6-ee3c-4e6c-9ce6-0061ed5ddc11")
-                val request = DownloadManager.Request(uri)
-
-                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-                request.setDestinationInExternalFilesDir(
-                    this,
-                    DIRECTORY_DOWNLOADS,
-                    "Cessna-172N-POH.pdf"
-                )
-                request.setDestinationInExternalPublicDir(
-                    DIRECTORY_DOWNLOADS,
-                    "Cessna-172N-POH.pdf"
-                )
-
-                dn.enqueue(request)
+                download()
             }
         }
+    }
+
+    private fun notification() {
+
+        val pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationChannel = NotificationChannel("com.gabrifermar.proyectodam","prueba",NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.GREEN
+            notificationChannel.enableVibration(false)
+            notificationManager.createNotificationChannel(notificationChannel)
+
+            builder = Notification.Builder(this,"com.gabrifermar.proyectodam")
+                .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark)
+                .setContentText("buenas tardes")
+                .setContentIntent(pendingIntent)
+        }else{
+
+            builder = Notification.Builder(this)
+                .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark)
+                .setContentIntent(pendingIntent)
+        }
+        notificationManager.notify(1234,builder.build())
+
+    }
+
+    private fun download() {
+        val dn: DownloadManager =
+            getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+        val uri =
+            Uri.parse("https://firebasestorage.googleapis.com/v0/b/proyectoaep-d6bc6.appspot.com/o/Cessna-172N-POH.pdf?alt=media&token=30bc84a6-ee3c-4e6c-9ce6-0061ed5ddc11")
+        val request = DownloadManager.Request(uri)
+
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+        request.setDestinationInExternalFilesDir(
+            this,
+            DIRECTORY_DOWNLOADS,
+            "Cessna-172N-POH.pdf"
+        )
+        request.setDestinationInExternalPublicDir(
+            DIRECTORY_DOWNLOADS,
+            "Cessna-172N-POH.pdf"
+        )
+
+        //start download
+        dn.enqueue(request)
+
+        //check when dowload finish to show notification
+        val broadcastReceiver=object :BroadcastReceiver(){
+            override fun onReceive(p0: Context?, p1: Intent?) {
+                notification()
+            }
+        }
+        registerReceiver(broadcastReceiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+
     }
 
     private fun checkPermission(): Boolean {
